e78bae49f1346a1ff150528222b9fe6570a15a2c
Leandro Soria <leandrosoria@hotmail.com>
2024-06-03 13:09:32 -0300

    Use a FSM to handle different states

--- app/src/main.c
+++ app/src/main.c
@@ -24,63 +24,78 @@ Some fancy copyright message here (if needed)
 #define CHAR_ESCAPE		0x1B	/* 'ESC' character is used as terminator */
 
 // === Private data type declarations ============================================================================== //
+
+typedef enum {
+	GETTING_KEY,
+	SETTING_KEY,
+	ECHOING,
+	ERROR,
+} ApplicationState;
+
 // === Private variable declarations =============================================================================== //
 // === Private function declarations =============================================================================== //
 
+static void reset_key(void);
 static void print_bar(void);
+static void on_error(void);
+static void on_getting_key(void);
+static void on_setting_key(void);
+static void on_echoing(void);
 
 // === Public variable definitions ================================================================================= //
 // === Private variable definitions ================================================================================ //
 
 static uint8_t key_buffer[16] = {0};
 
+static ApplicationState current_state;
+
 // === Private function implementation ============================================================================= //
 
-static void print_bar(void) { xil_printf("----------------------------------------\n\r"); }
+static void reset_key(void) { memset(key_buffer, 0, 16); }
 
-// === Public function implementation ============================================================================== //
+static void print_bar(void) { xil_printf("----------------------------------------\r\n"); }
 
-int main(void)
+static void on_error(void) { while(1); }
+
+static void on_getting_key(void)
 {
-	char running = 1;
-	char echoing = 1;
-	int index = 0;
-	u8 RecvChar;
+	static uint8_t show_message = 1;
+	static uint8_t index = 0;
+	uint8_t received_char = 0;
 
-	if (uart_initialize(UART_CRYPTO, UART_CRYPTO_INSTANCE) == XST_FAILURE) {
-		xil_printf("UART Crypto Initialization Failed\r\n");
-		return XST_FAILURE;
-	}
-	if (uart_initialize(UART_SHELL, UART_SHELL_INSTANCE) == XST_FAILURE) {
-		xil_printf("UART Shell Initialization Failed\r\n");
-		return XST_FAILURE;
+	if(show_message) {
+		xil_printf("Enter the AES-128 Key: ");
+		show_message = 0;
 	}
 
-	print_bar();
-	xil_printf("AES-128 Encoder application starts here\n\r");
-	print_bar();
-	xil_printf("Enter the AES-128 Key: ");
+	received_char = uart_read_byte(UART_SHELL);
+	if (CHAR_ENTER == received_char) {
+		current_state = SETTING_KEY;
+		index = 0;
+		show_message = 1;
+		return;
 
-	while(running) {
-		RecvChar = uart_read_byte(UART_SHELL);
-		if (CHAR_ENTER == RecvChar) {
-			running = 0;
-		} else {
-			key_buffer[index++] = RecvChar;
+	} else {
+		key_buffer[index++] = received_char;
 
-			if(index == 16) {
-				running = FALSE;
-			}
+		if(index == 16) {
+			current_state = SETTING_KEY;
+			index = 0;
+			show_message = 1;
+			return;
 		}
-
-		// Echo the character back
-		uart_write_byte(UART_SHELL, RecvChar);
 	}
 
+	// Echo the character back
+	uart_write_byte(UART_SHELL, received_char);
+}
+
+static void on_setting_key(void)
+{
 	uart_write_bytes(UART_SHELL, (uint8_t*)"\r\n", 2);
 
 	xil_printf("Setting key: %s ( ", key_buffer);
-	for(uint8_t i = 0; i < 17; i++) {
+	for(uint8_t i = 0; i < 16; i++) {
 		xil_printf("%02x ", key_buffer[i]);
 	}
 	xil_printf(")\r\n");
@@ -89,14 +104,62 @@ int main(void)
 	xil_printf("Messages typed here will be sent encrypted using the specified key.\r\n");
 	print_bar();
 
-	while(echoing) {
-		RecvChar = uart_read_byte(UART_SHELL);
-		if (CHAR_ESCAPE == RecvChar) {
-			echoing = 0;
+	current_state = ECHOING;
+}
+
+static void on_echoing(void)
+{
+	uint8_t received_char = uart_read_byte(UART_SHELL);
+	if (CHAR_ESCAPE == received_char) {
+		current_state = GETTING_KEY;
+		reset_key();
+		xil_printf("\033[2J\033[H");
+		return;
+	}
+	// Echo the character back
+	uart_write_byte(UART_SHELL, received_char);
+	uart_write_byte(UART_CRYPTO, received_char);
+}
+
+// === Public function implementation ============================================================================== //
+
+int main(void)
+{
+	if (uart_initialize(UART_CRYPTO, UART_CRYPTO_INSTANCE) == XST_FAILURE) {
+		xil_printf("UART Crypto Initialization Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	if (uart_initialize(UART_SHELL, UART_SHELL_INSTANCE) == XST_FAILURE) {
+		xil_printf("UART Shell Initialization Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	print_bar();
+	xil_printf("AES-128 Encoder application starts here\n\r");
+	print_bar();
+
+	current_state = GETTING_KEY;
+
+	while(1) {
+		switch (current_state)
+		{
+		case GETTING_KEY:
+			on_getting_key();
+			break;
+		case SETTING_KEY:
+			on_setting_key();
+			break;
+		case ECHOING:
+			on_echoing();
+			break;
+		case ERROR:
+			on_error();
+			break;
+		default:
+			on_error();
+			break;
 		}
-		// Echo the character back
-		uart_write_byte(UART_SHELL, RecvChar);
-		uart_write_byte(UART_CRYPTO, RecvChar);
 	}
 
 	print_bar();
